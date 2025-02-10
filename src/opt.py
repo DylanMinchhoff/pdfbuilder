c2c40080830e861390645e4f076d5ad4a962120b
DylanMinchhoff <dylanminchhoff@gmail.com>
2025-02-10 11:30:21 -0500

    all passing

--- src/opt.py
+++ src/opt.py
@@ -1,5 +1,6 @@
 from functools import partial
 from kernel import Expression, Int, Add, Subtract, Multiply, Let, Var
+from eval import eval_expr
 
 
 def opt_expr(
@@ -7,6 +8,7 @@ def opt_expr(
 ) -> Expression:
     recur = partial(opt_expr)
 
+    # return as val, var for optimization
     match expr:
         case Int():
             return expr
@@ -15,6 +17,12 @@ def opt_expr(
             match recur(e1), recur(e2):
                 case [Int(i1), Int(i2)]:
                     return Int(i1 + i2)
+                case [Int(a), Add(Int(b), ex)]:
+                    return Add(Int(a + b), ex)
+                case [Add(Int(a), e1), Add(Int(b), e2)]:
+                    return Add(Int(a + b), Add(e1, e2))
+                case [e1, Int(a)]:
+                    return Add(Int(a), e1)
                 case [e1, e2]:  # pragma: no branch
                     return Add(e1, e2)
 
@@ -29,11 +37,21 @@ def opt_expr(
             match recur(e1), recur(e2):
                 case [Int(i1), Int(i2)]:
                     return Int(i1 * i2)
+                case [Int(a), Multiply(Int(b), ex)]:
+                    return Multiply(Int(a * b), ex)
+                case [Multiply(Int(a), e1), Multiply(Int(b), e2)]:
+                    return Multiply(Int(a * b), Multiply(e1, e2))
+                case [e1, Int(a)]:
+                    return Multiply(Int(a), e1)
                 case [e1, e2]:  # pragma: no branch
+                    # bring var to right and value to left [val, var]
                     return Multiply(e1, e2)
 
         case Let(x, e1, e2):
-            raise NotImplementedError()
+            # if var is const replace with const
+            if e2 == Var(x):
+                return e1
+            return Let(x, e1, e2)
 
         case Var(x):  # pragma: no branch
-            raise NotImplementedError()
+            return Var(x)
